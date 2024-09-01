e8270fd0cc21d67fc694bb2924b377c6a8cc895f
reichman11 <domino.reichy@gmail.com>
2024-09-01 10:01:53 +0200

    Zachycení eventlisteneru na Add To Cart tlačítko, změna html v tlačítku a přičítání nebo odečítání položek

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const cartIcon = document.querySelector(".cart-icon");
 const closeCart = document.querySelector(".close-btn");
 const cart = document.querySelector(".cart-wrapper");
 
+let counter = 1;
+
 //Vyjížděcí menu
 cartIcon.addEventListener("click", () => {
   cart.classList.toggle("show-cart");
@@ -9,3 +11,29 @@ cartIcon.addEventListener("click", () => {
 closeCart.addEventListener("click", () => {
   cart.classList.toggle("show-cart");
 });
+
+const addToCartBtn = document.querySelector(".add-to-cart-btn");
+
+addToCartBtn.addEventListener("click", () => {
+  addToCartBtn.innerHTML = `<div class="add-to-cart-options">
+                              <span class="add-item">+</span>
+                              <span class="number-of-items">${counter}</span>
+                              <span class="remove-item">-</span>
+                            </div>`;
+  plusItem();
+  minutItem();
+});
+
+const plusItem = () => {
+  const addItem = document.querySelector(".add-item");
+  addItem.addEventListener("click", () => {
+    counter++;
+  });
+};
+
+const minutItem = () => {
+  const removeItem = document.querySelector(".remove-item");
+  removeItem.addEventListener("click", () => {
+    counter--;
+  });
+};
