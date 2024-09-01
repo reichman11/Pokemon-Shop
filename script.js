cd7113a83e16d2660a16ec56636d8787960da96c
reichman11 <domino.reichy@gmail.com>
2024-09-01 10:51:13 +0200

    Přídání volby počtu položek, změna na původni obsah tlačíkta při žádné položce

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const closeCart = document.querySelector(".close-btn");
 const cart = document.querySelector(".cart-wrapper");
 
 let counter = 1;
+let cartCounter = document.querySelector(".item-counter");
+const cartItem = document.querySelector(".cart-item");
 
 //Vyjížděcí menu
 cartIcon.addEventListener("click", () => {
@@ -12,18 +14,6 @@ closeCart.addEventListener("click", () => {
   cart.classList.toggle("show-cart");
 });
 
-const addToCartBtn = document.querySelector(".add-to-cart-btn");
-
-addToCartBtn.addEventListener("click", () => {
-  addToCartBtn.innerHTML = `<div class="add-to-cart-options">
-                              <span class="add-item">+</span>
-                              <span class="number-of-items">${counter}</span>
-                              <span class="remove-item">-</span>
-                            </div>`;
-  plusItem();
-  minutItem();
-});
-
 const plusItem = () => {
   const addItem = document.querySelector(".add-item");
   addItem.addEventListener("click", () => {
@@ -31,9 +21,38 @@ const plusItem = () => {
   });
 };
 
-const minutItem = () => {
+const minusItem = () => {
   const removeItem = document.querySelector(".remove-item");
   removeItem.addEventListener("click", () => {
-    counter--;
+    if (counter > 0) {
+      counter--;
+    }
+    if (counter <= 0) {
+      setTimeout(changeToAddToCartBtn, 10); // nastaveno zpoždení, aby se mohla provest changeContent funkce.
+    }
   });
 };
+
+const changeToAddToCartBtn = () => {
+  addToCartBtn.innerHTML = `<img src="images/pokeball.png" alt="pokeball-image" />Add to
+  Cart`;
+  counter = 1;
+};
+
+const changeToChoiceOfItems = () => {
+  addToCartBtn.innerHTML = `<div class="add-to-cart-options">
+                                <span class="add-item">+</span>
+                                <span class="number-of-items">${counter}</span>
+                                <span class="remove-item">-</span>
+                              </div>`;
+
+  cartItem.classList.toggle("show-cart-item");
+
+  plusItem();
+  minusItem();
+};
+
+const addToCartBtn = document.querySelector(".add-to-cart-btn");
+addToCartBtn.addEventListener("click", () => {
+  changeToChoiceOfItems();
+});
