4edd09b7487317d8243a2a0187f5be20b0b9cf29
reichman11 <domino.reichy@gmail.com>
2024-09-07 17:45:55 +0200

    Přidání updateItemCounter funkce, změna počítadla pri klikání v košíku + změna celkové ceny

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const closeCart = document.querySelector(".close-btn");
 const cart = document.querySelector(".cart-wrapper");
 
 let counter = 1;
+
 let cartCounter = document.querySelector(".item-counter");
 const cartItem = document.querySelector(".cart-item");
 
@@ -15,22 +16,28 @@ closeCart.addEventListener("click", () => {
 });
 
 const plusItem = () => {
-  const addItem = document.querySelector(".add-item");
-  addItem.addEventListener("click", () => {
-    counter++;
-    cartItem.classList.remove("show-cart-item");
+  const addItem = document.querySelectorAll(".add-item");
+  addItem.forEach((e) => {
+    e.addEventListener("click", () => {
+      counter++;
+      cartItem.classList.remove("show-cart-item");
+      updateItemCounter();
+    });
   });
 };
 
 const minusItem = () => {
-  const removeItem = document.querySelector(".remove-item");
-  removeItem.addEventListener("click", () => {
-    if (counter > 0) {
-      counter--;
-    }
-    if (counter <= 0) {
-      setTimeout(changeToAddToCartBtn, 10); // nastaveno zpoždení, aby se mohla provest changeContent funkce.
-    }
+  const removeItem = document.querySelectorAll(".remove-item");
+  removeItem.forEach((e) => {
+    e.addEventListener("click", () => {
+      if (counter > 0) {
+        counter--;
+        updateItemCounter();
+      }
+      if (counter <= 0) {
+        setTimeout(changeToAddToCartBtn, 10); // nastaveno zpoždení, aby se mohla provest changeContent funkce.
+      }
+    });
   });
 };
 
@@ -39,9 +46,21 @@ const changeToAddToCartBtn = () => {
   Cart`;
   cartItem.classList.add("show-cart-item");
   counter = 1;
+  updateItemCounter();
 };
 
+const addToCartBtn = document.querySelector(".add-to-cart-btn");
+addToCartBtn.addEventListener("click", () => {
+  cartItem.classList.remove("show-cart-item");
+  changeToChoiceOfItems();
+});
+
+const addRemoveBox = document.querySelector(".add-remove-box");
+
 const changeToChoiceOfItems = () => {
+  addRemoveBox.innerHTML = `<span class="add-item">+</span>
+                            <span class="number-of-items">${counter}</span>
+                            <span class="remove-item">-</span>`;
   addToCartBtn.innerHTML = `<div class="add-to-cart-options">
                                 <span class="add-item">+</span>
                                 <span class="number-of-items">${counter}</span>
@@ -52,8 +71,51 @@ const changeToChoiceOfItems = () => {
   minusItem();
 };
 
-const addToCartBtn = document.querySelector(".add-to-cart-btn");
-addToCartBtn.addEventListener("click", () => {
-  cartItem.classList.remove("show-cart-item");
-  changeToChoiceOfItems();
-});
+const updateItemCounter = () => {
+  // Aktualizování položek
+  const totalProductPrice = document.querySelector(".total-product-price");
+  const productPrice = document.querySelector(".product-price");
+
+  const numberOfItems = document.querySelectorAll(".number-of-items");
+  numberOfItems.forEach((e) => {
+    e.textContent = counter;
+  });
+
+  let totalPrice = parseInt(productPrice.textContent) * counter;
+  totalProductPrice.innerHTML = `$ ${totalPrice}`;
+};
+
+// // Vytvoření funkce pro nahrání dalších dat ze souboru .json
+// const initApp = () => {};
+// //získání dat z .json
+// fetch("products.json")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     listOfProducts = data;
+
+//     addDataToHTML();
+//   });
+
+// initApp();
+
+// // přidání dat do HTML
+// const addDataToHTML = () => {
+//   const productListHTML = document.querySelector(".product-card-box");
+//   productListHTML.innerHTML = "";
+//   if (listOfProducts.length > 0) {
+//     listOfProducts.forEach((product) => {
+//       const newProduct = document.createElement("div");
+//       newProduct.classList.add("product-card");
+//       newProduct.innerHTML = `<div class="product-card-image">
+//                                 <img src="images/${product.image}" alt="${product.name} image" />
+//                                 <button class="add-to-cart-btn item-chooser">
+//                                 <img src="images/pokeball.png" alt="pokeball-image" />Add to Cart </button>
+//                               </div>
+//                               <div class="product-text">
+//                                 <p class="name-of-pokemon">${product.name}</p>
+//                                 <p class="price-of-pokemon">${product.price}</p>
+//                               </div>`;
+//       productListHTML.appendChild(newProduct);
+//     });
+//   }
+// };
