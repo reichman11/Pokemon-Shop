37ef971d4028c87773df3d1901cb8aee6b1e53ba
reichman11 <domino.reichy@gmail.com>
2024-09-14 02:09:52 +0200

    kompletní funkční kód pro pridání, odebrání a součet cen produktů

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const cartIcon = document.querySelector(".cart-icon");
 const closeCart = document.querySelector(".close-btn");
 const cart = document.querySelector(".cart-wrapper");
 
-let cartCounter = document.querySelector(".item-counter");
-
 // Mapa pro uložení počítadel jednotlivých produktů
 const productCounters = {};
 
@@ -36,14 +34,14 @@ const addDataToHTML = () => {
     const newProduct = document.createElement("div");
     newProduct.classList.add("product-card");
     newProduct.innerHTML = `<div class="product-card-image">
-                              <img src="images/${product.image}" alt="${product.name} image" data-product-id="${product.id}" />
-                              <button class="add-to-cart-btn" data-product-id="${product.id}">
+                              <img src="images/${product.image}" alt="${product.name} image" product-id="${product.id}" />
+                              <button class="add-to-cart-btn" product-id="${product.id}">
                                 <img src="images/pokeball.png" alt="pokeball-image" />Add to Cart
                               </button>
                             </div>
                             <div class="product-text">
-                              <p class="name-of-pokemon" data-product-id="${product.id}">${product.name}</p>
-                              <p class="price-of-pokemon" data-product-id="${product.id}">$ ${product.price}</p>
+                              <p class="name-of-pokemon" product-id="${product.id}">${product.name}</p>
+                              <p class="price-of-pokemon" product-id="${product.id}">$ ${product.price}</p>
                             </div>`;
 
     productListHTML.appendChild(newProduct);
@@ -60,36 +58,32 @@ const attachEventListeners = () => {
 
   addToCartButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
-      const productId = e.target
-        .closest("button")
-        .getAttribute("data-product-id");
-      changeToChoiceOfItems(e.target.closest("button"), productId);
-      addToCart(productId); // Přidání položky do košíku
+      const id = e.target.closest("button").getAttribute("product-id");
+      changeToChoiceOfItems(e.target.closest("button"), id);
+      addToCart(id); // Přidání položky do košíku
     });
   });
 };
 
 // Přidání položky do košíku
-const addToCart = (productId) => {
+const addToCart = (id) => {
   const cartItemsList = document.querySelector(".cart-items-list");
-  let cartItem = document.querySelector(
-    `.cart-item[data-product-id="${productId}"]`
-  );
+  let cartItem = document.querySelector(`.cart-item[product-id="${id}"]`);
 
   if (!cartItem) {
     cartItem = document.createElement("div");
     cartItem.classList.add("cart-item");
-    cartItem.setAttribute("data-product-id", productId);
+    cartItem.setAttribute("product-id", id);
 
-    const product = products.find((p) => p.id == productId);
+    const product = products.find((p) => p.id == id);
     cartItem.innerHTML = `<div>
-                            <img src="images/${product.preview}" alt="#" data-product-id="${product.id}" />
+                            <img src="images/${product.preview}" alt="#" product-id="${product.id}" />
                           </div>
                           <p class="product-name">${product.name}</p>
-                          <p class="total-product-price" data-product-id="${product.id}">$ ${product.price}</p>
+                          <p class="total-product-price" product-id="${product.id}">$ ${product.price}</p>
                           <div class="add-remove-box">
                             <span class="add-item">+</span>
-                            <span class="item-counter" data-product-id="${product.id}">${productCounters[productId]}</span>
+                            <span class="item-counter" product-id="${id}">${productCounters[id]}</span>
                             <span class="remove-item">-</span>
                           </div>`;
 
@@ -97,23 +91,23 @@ const addToCart = (productId) => {
 
     // Přidání posluchačů událostí na tlačítka "+" a "-"
     cartItem.querySelector(".add-item").addEventListener("click", () => {
-      updateCounter(productId, 1);
+      updateCounter(id, 1);
     });
 
     cartItem.querySelector(".remove-item").addEventListener("click", () => {
-      updateCounter(productId, -1);
+      updateCounter(id, -1);
     });
   }
+  updateTotalPrice();
 };
-
 // Změna tlačítka na volbu položek
-const changeToChoiceOfItems = (button, productId) => {
-  const counter = productCounters[productId]; // Získání počítadla pro konkrétní produkt
+const changeToChoiceOfItems = (button, id) => {
+  const counter = productCounters[id]; // Získání počítadla pro konkrétní produkt
 
   button.innerHTML = `
     <div class="add-to-cart-options">
       <span class="add-item">+</span>
-      <span class="number-of-items" data-product-id="${productId}">${counter}</span>
+      <span class="number-of-items" product-id="${id}">${counter}</span>
       <span class="remove-item">-</span>
     </div>`;
 
@@ -123,33 +117,32 @@ const changeToChoiceOfItems = (button, productId) => {
 
   // Přidání event listenerů pro tlačítka "+", "-"
   addItemBtn.addEventListener("click", () => {
-    button.querySelector(".number-of-items").textContent =
-      productCounters[productId]; // Aktualizace zobrazení počtu položek
-    updateCounter(productId, 1); // Zvýšíme o 1
+    button.querySelector(".number-of-items").textContent = productCounters[id]; // Aktualizace zobrazení počtu položek
+    updateCounter(id, 1); // Zvýšíme o 1
   });
 
   removeItemBtn.addEventListener("click", () => {
-    updateCounter(productId, -1); // Snížíme o 1
+    updateCounter(id, -1); // Snížíme o 1
 
-    if (productCounters[productId] <= 0) {
-      changeBtnToAddToCart(button, productId); // Pokud je počet 0, změň tlačítko zpět na původní
-      removeItemFromCart(productId); // Odstraň položku z košíku
+    if (productCounters[id] <= 0) {
+      changeBtnToAddToCart(button, id); // Pokud je počet 0, změň tlačítko zpět na původní
+      removeItemFromCart(id); // Odstraň položku z košíku
     } else {
       button.querySelector(".number-of-items").textContent =
-        productCounters[productId]; // Aktualizace zobrazení počtu položek
+        productCounters[id]; // Aktualizace zobrazení počtu položek
     }
   });
 };
 
 // Změna tlačítka zpět na "Add to Cart"
-const changeBtnToAddToCart = (button, productId) => {
-  productCounters[productId] = 1; // Restart počítadla
+const changeBtnToAddToCart = (button, id) => {
+  productCounters[id] = 1; // Restart počítadla
   button.innerHTML = `<img src="images/pokeball.png" alt="pokeball-image" />Add to Cart`;
 };
 
 // Aktualizace počtu položek
-const updateCounter = (productId, change) => {
-  let currentCount = productCounters[productId];
+const updateCounter = (id, change) => {
+  let currentCount = productCounters[id];
   currentCount += change;
 
   // Zabráníme záporným hodnotám
@@ -158,40 +151,75 @@ const updateCounter = (productId, change) => {
 
     // Změna tlačítka zpět na "Add to Cart", pokud je počet 0
     const productCardButton = document.querySelector(
-      `.add-to-cart-btn[data-product-id="${productId}"]`
+      `.add-to-cart-btn[product-id="${id}"]`
     );
     if (productCardButton) {
-      changeBtnToAddToCart(productCardButton, productId);
+      changeBtnToAddToCart(productCardButton, id);
     }
 
     // Odstranění položky z košíku
-    removeItemFromCart(productId);
+    removeItemFromCart(id);
   }
 
-  productCounters[productId] = currentCount; // Aktualizace počítadla pro konkrétní produkt
+  productCounters[id] = currentCount; // Aktualizace počítadla pro konkrétní produkt
 
   // Aktualizace počítadla pro košík
-  productCounters[productId] = currentCount;
+  productCounters[id] = currentCount;
   const cartItemCounter = document.querySelector(
-    `.cart-item[data-product-id="${productId}"] .item-counter`
-  );
-  // Aktualizace počítadla na tlačítku produktové karty
-  const productCardCounter = document.querySelector(
-    `.add-to-cart-btn[data-product-id="${productId}"] .number-of-items`
+    `.item-counter[product-id="${id}"]`
   );
 
-  if (cartItemCounter && productCardCounter) {
+  // Aktualizace počítadla na tlačítku produktové karty
+  const btnItemCounter = document.querySelector(
+    `.number-of-items[product-id="${id}"]`
+  );
+
+  if (cartItemCounter && btnItemCounter) {
     cartItemCounter.textContent = currentCount;
-    productCardCounter.textContent = currentCount;
+    btnItemCounter.textContent = currentCount;
   }
+
+  //Součet ceny pro jednotlivé položky
+  const totalProductPrice = document.querySelector(
+    `.total-product-price[product-id="${id}"]`
+  );
+  const productPrice = document.querySelector(
+    `.price-of-pokemon[product-id="${id}"]`
+  );
+
+  let productPriceNumber = productPrice.textContent.replace("$", "").trim();
+  productPriceNumber = parseInt(productPriceNumber);
+
+  let totalPrice = parseInt(productPriceNumber * cartItemCounter.textContent);
+  totalProductPrice.innerHTML = `$ ${totalPrice}`;
+
+  updateTotalPrice();
+};
+
+// Aktualizace celkové ceny
+const updateTotalPrice = () => {
+  let totalPrice = 0;
+
+  // Získání všech cen jednotlivých produktů
+  const productPrices = document.querySelectorAll(".total-product-price");
+
+  productPrices.forEach((priceElement) => {
+    const priceText = priceElement.textContent.replace("$", "").trim();
+    const price = parseFloat(priceText);
+
+    totalPrice += price; // Přičtení ceny k celkové částce
+  });
+
+  // Zobrazení celkové ceny v košíku
+  const totalCartPriceElement = document.querySelector(".total-cart-price");
+  totalCartPriceElement.textContent = `Total price: $ ${totalPrice}`;
 };
 
 // Odstranění položky z košíku
-const removeItemFromCart = (productId) => {
-  const cartItem = document.querySelector(
-    `.cart-item[data-product-id="${productId}"]`
-  );
+const removeItemFromCart = (id) => {
+  const cartItem = document.querySelector(`.cart-item[product-id="${id}"]`);
   if (cartItem) {
     cartItem.remove();
   }
+  updateTotalPrice();
 };
